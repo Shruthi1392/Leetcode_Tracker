8aaca7bdeb1c888adb2431447a1ff1e64dd7367a
Shruthi1392 <shruthisrinivas1392@gmail.com>
2025-10-17 06:02:27 +0530

    Only fetches user stats.

--- services/leetcodeClient.js
+++ services/leetcodeClient.js
@@ -6,68 +6,33 @@ const USER_STATS_QUERY = `
 query userProfile($username: String!) {
   matchedUser(username: $username) {
     username
+    profile {
+      realName
+      reputation
+      ranking
+    }
     submitStatsGlobal {
       acSubmissionNum {
         difficulty
         count
-        submissions
       }
     }
-    recentAcSubmissionList(limit: 1000) {
-      titleSlug
-      title
-      difficulty
-    }
   }
 }
 `;
-
-const ALL_PROBLEMS_QUERY = `
-query problemsetQuestionList {
-  problemsetQuestionList: questionList {
-    questions: data {
-      title
-      titleSlug
-      difficulty
-    }
-  }
-}
-`;
-
-const headers = {
-  'Content-Type': 'application/json',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-  'Accept': 'application/json',
-  'Referer': 'https://leetcode.com',
-  'Origin': 'https://leetcode.com',
-};
 
 async function getPublicProfile(username) {
   try {
     const resp = await axios.post(
       LEETCODE_GRAPHQL,
       { query: USER_STATS_QUERY, variables: { username } },
-      { headers }
+      { headers: { 'Content-Type': 'application/json' } }
     );
     return resp.data.data;
   } catch (err) {
-    console.error("GraphQL fetch failed:", err.response?.data || err.message);
+    console.error(err);
     throw new Error('Failed to fetch LeetCode profile');
   }
 }
 
-async function getAllProblems() {
-  try {
-    const resp = await axios.post(
-      LEETCODE_GRAPHQL,
-      { query: ALL_PROBLEMS_QUERY },
-      { headers }
-    );
-    return resp.data.data.problemsetQuestionList.questions;
-  } catch (err) {
-    console.error("GraphQL fetch failed:", err.response?.data || err.message);
-    throw new Error('Failed to fetch problem list from LeetCode');
-  }
-}
-
-module.exports = { getPublicProfile, getAllProblems };
+module.exports = { getPublicProfile };
